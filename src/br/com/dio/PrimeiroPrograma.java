7a75c7f6f02b78ae1880912d98464d594d5c5db4
Leonam Souza <Leonam@192.168.0.75>
2022-08-15 21:50:02 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,27 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Livro livro = new Livro();
+		Gato gato = new Gato();
+		
+		
+		System.out.println(gato);
+		System.out.println(livro);
+		
+		/*int a = 3;
+		int b = 2;		
+		System.out.println("Hello world! " + (a+b));*/
+	}
+
+}
+
+
+class Livro{
+	private String nome;
+	private String npag;
+}
